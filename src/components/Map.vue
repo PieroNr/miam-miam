40c7ab304b34862dbe1b52591f58f2944c51747d
Piero <piero.neri@hotmail.fr>
2022-12-06 11:35:37 +0100

    add fix charac points

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,6 +5,9 @@
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import benoit from "@/assets/img/avatar1.png";
+import akim from "@/assets/img/avatar2.png";
+import sophie from "@/assets/img/avatar3.png";
 
 
 export default {
@@ -24,7 +27,23 @@ export default {
     var customPane = this.map.createPane("customPane");
     var canvasRenderer = L.canvas({ pane: "customPane" });
     customPane.style.zIndex = 399; // put just behind the standard overlay pane which is at 400
+    var avatar1 = L.icon({
+      iconUrl: benoit,
+      iconSize:     [35, 35],
+    });
 
+    var avatar2 = L.icon({
+      iconUrl: akim,
+      iconSize:     [35, 35],
+    });
+
+    var avatar3 = L.icon({
+      iconUrl: sophie,
+      iconSize:     [35, 35],
+    });
+    L.marker([48.86, 2.33], {icon: avatar1}).addTo(this.map);
+    L.marker([48.84, 2.35], {icon: avatar2}).addTo(this.map);
+    L.marker([48.85, 2.37], {icon: avatar3}).addTo(this.map);
 
   },
   onBeforeUnmount() {
